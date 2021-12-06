d5010a1d96818396a8d2cf9d9b7f6e8962a89ba0
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-12-06 10:43:34 +0300

    first commit

---
+++ app/src/main/java/com/chalie/newsdeck/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chalie.newsdeck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
