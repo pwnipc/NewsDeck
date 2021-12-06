705a7b91cc729b2e71beccc7573b64b19f454890
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-12-06 13:50:36 +0300

    add api key access method

---
+++ app/src/main/java/com/chalie/newsdeck/Constants.java
@@ -0,0 +1,7 @@
+package com.chalie.newsdeck;
+
+public class Constants {
+    public static final String BASE_URL = "https://newsapi.org/v2/everything";
+    public static final String NEWSAPI_API_KEY = BuildConfig.NEWSAPI_API_KEY;
+
+}
