5cc0435da1cbe37315eaab38f937cbeab1f37b75
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-12-08 02:00:09 +0300

    add pojo models

---
+++ app/src/main/java/com/chalie/newsdeck/models/Source.java
@@ -0,0 +1,50 @@
+
+package com.chalie.newsdeck.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Source {
+
+    @SerializedName("id")
+    @Expose
+    private Object id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Source() {
+    }
+
+    /**
+     * 
+     * @param name
+     * @param id
+     */
+    public Source(Object id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Object getId() {
+        return id;
+    }
+
+    public void setId(Object id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
