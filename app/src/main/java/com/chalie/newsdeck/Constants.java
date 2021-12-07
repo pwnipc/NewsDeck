0524322e0cdffbf6d92c543a749512aab95c1ea0
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-12-08 02:03:34 +0300

    fix a typo

--- app/src/main/java/com/chalie/newsdeck/Constants.java
+++ app/src/main/java/com/chalie/newsdeck/Constants.java
@@ -1,7 +1,7 @@
 package com.chalie.newsdeck;
 
 public class Constants {
-    public static final String BASE_URL = "https://newsapi.org/v2/everything";
-    public static final String NEWSAPI_API_KEY = BuildConfig.NEWSAPI_API_KEY;
+    public static final String BASE_URL = "https://newsapi.org/v2/";
+    public static final String NEWSAPI_KEY = BuildConfig.NEWSAPI_KEY;
 
 }
