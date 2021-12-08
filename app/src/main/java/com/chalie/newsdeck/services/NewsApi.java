ccaa11b33d48c311d77750178421f9af646a9488
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-12-08 18:05:54 +0300

    refactor query parameters

--- app/src/main/java/com/chalie/newsdeck/services/NewsApi.java
+++ app/src/main/java/com/chalie/newsdeck/services/NewsApi.java
@@ -1,16 +1,22 @@
 package com.chalie.newsdeck.services;
 
+import static com.chalie.newsdeck.Constants.NEWSAPI_KEY;
+
+import com.chalie.newsdeck.models.Article;
 import com.chalie.newsdeck.models.Everything;
-import com.chalie.newsdeck.models.Source;
+
+import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 public interface NewsApi {
-    @GET("everything?q={query}")
-    Call<Everything> getArticles(
-            @Query("query") String query
-    );
+    @GET("everything")
+    Call<Everything> getArticles(@Query("q") String query, @Query("apiKey") String apiKey);
 }
